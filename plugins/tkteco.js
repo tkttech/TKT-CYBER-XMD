94ca617190e4faf2c4375fcbd5665a650d68f7e8
tkttech <tkttech09@gmail.com>
2025-07-05 08:34:20 +0200

    Add files via upload

---
+++ plugins/tkteco.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const { cmd } = require("../command");
+
+
+cmd({
+    pattern: "mpesamenu",
+    alias: ["pesa"],
+    desc: "menu the bot",
+    category: "menu",
+    react: "🎀",
+    filename: __filename
+}, 
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let dec = `*╭───★BUY ME A SUPER 🍺 ★*
+★‎├† .ecocash*
+‎★├† .airtime*
+‎★╰───────────────❍*`;
+
+        await conn.sendMessage(
+            from,
+            {
+                image: { url: `https://files.catbox.moe/1cp4yq.jpeg` },
+                caption: dec,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363418027651738@newsletter',
+                        newsletterName: "TKT-CYBER-XMD PAYMENT",
+                        serverMessageId: 143
+                    }
+                }
+            },
+            { quoted: mek }
+        );
+
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
